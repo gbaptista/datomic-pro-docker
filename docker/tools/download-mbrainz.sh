5afca3b34ec269fefc5652a540ea10ae7d17857f
gbaptista <guilhermebaptistasilva@gmail.com>
2024-10-06 09:41:45 -0300

    fixing md5 checksum

--- docker/tools/download-mbrainz.sh
+++ docker/tools/download-mbrainz.sh
@@ -11,8 +11,10 @@ if [ ! -f "/usr/mbrainz.ready" ]; then
     https://s3.amazonaws.com/mbrainz/datomic-mbrainz-1968-1973-backup-2017-07-20.tar \
     -o datomic-mbrainz-backup.tar
 
-  echo "Checking integrity of downloaded file..."
-  echo "063a8b80e53023f0c7694792230b72a6  datomic-mbrainz-backup.tar" | md5sum -c -
+  echo "Checking integrity of downloaded file... [063a8b80e53023f0c7694792230b72a6]"
+  echo "063a8b80e53023f0c7694792230b72a6  datomic-mbrainz-backup.tar" \
+    | md5sum -c - \
+    || exit 1
 
   echo "Extracting downloaded file..."
   tar -xvf datomic-mbrainz-backup.tar -C /usr/
